413702454c147d5849d8c12944c9a5e440c2a7b9
arora_72 <indresh.arora2016@vitstudent.ac.in>
2017-06-10 03:39:09 +0530

    Initial Commit

---
+++ chat app firebase/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  chat app firebase
+//
+//  Created by arora_72 on 10/06/17.
+//  Copyright © 2017 indresh arora. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
